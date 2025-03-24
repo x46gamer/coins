d480762d0a852595e36f316830e640ec1e48aca7
x46gamer <123272999+x46gamer@users.noreply.github.com>
2025-03-24 17:45:44 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
+  apiKey: "AIzaSyDYBQxVlY1jqrBH6WQeGRoX5AJkVZXQBYo",
+  authDomain: "currency-exchange-community.firebaseapp.com",
+  projectId: "currency-exchange-community",
+  storageBucket: "currency-exchange-community.appspot.com",
+  messagingSenderId: "1098342071540",
+  appId: "1:1098342071540:web:b0c0e1c9c9b9b9b9b9b9b9",
+  databaseURL: "https://currency-exchange-community-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
